1d28f9a4d59f8151a8b20843dbbf0dfdcb2bd79c
Dheo Apriansyah <dheoap21@gmail.com>
2020-12-27 11:29:28 +0700

    merubah nav

--- application/views/templates_anggota/header.php
+++ application/views/templates_anggota/header.php
@@ -6,33 +6,33 @@
   <title>Dashboard - Aplikasi Perpustakaan</title>
   <link rel="stylesheet" href="<?= base_url().'assets/css/bootstrap.min.css'?>">
   <link rel="stylesheet" href="<?= base_url().'assets/datatable/datatables.css'?>">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
   
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <span class="navbar-brand active"><a class="nav-link" href="<?= base_url().'admin';?>">Perpustakaan</a></span>
+    <span class="navbar-brand active">Perpustakaan</span>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="<?= base_url().'admin/buku';?>">Data Buku</a>
+          <a class="nav-link" href="<?= base_url();?>">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?= base_url();?>admin/anggota">Data anggota</a>
+          <a class="nav-link" href="<?= base_url();?>"><i class="fas fa-cart-arrow-down"></i> Keranjang Pinjam</a>
         </li>
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link" href="<?= base_url();?>admin/peminjaman">Transaksi Peminjaman</a>
-        </li>
+        </li> -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <?= "Hallo, ".$this->session->userdata('nama_lengkap');?>
           </a>
           <div class="dropdown-menu p-2 bg-dark" aria-labelledby="navbarDropdownMenuLink">
-            <a class="nav-link" href="<?= base_url();?>admin/laporan">Laporan</a>
             <a class="nav-link" href="<?= base_url();?>admin/ganti_password">Ganti Password</a>
             <a class="nav-link text-danger" href="<?= base_url('auth/logout');?>">Logout</a>
           </div>
