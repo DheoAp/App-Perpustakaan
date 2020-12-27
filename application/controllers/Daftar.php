a942b231a4f9d3286fd2a4560acdf8463828bfe9
Dheo Apriansyah <dheoap21@gmail.com>
2020-12-27 10:53:28 +0700

    menambah title

--- application/controllers/Daftar.php
+++ application/controllers/Daftar.php
@@ -5,7 +5,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Daftar extends CI_Controller{
   public function index()
   {
-    $this->load->view('daftar');
+    $judul['title'] = "Daftar Anggota";
+    $this->load->view('daftar',$judul);
   }
 
 } // akhir class
