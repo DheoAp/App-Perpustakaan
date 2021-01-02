3ca6577cd54e23df72e19d2f57575fa230b96334
Dheo Apriansyah <dheoap21@gmail.com>
2021-01-02 15:47:09 +0700

    merubah table keranjang

--- application/views/anggota/keranjang_buku.php
+++ application/views/anggota/keranjang_buku.php
@@ -1,5 +1,5 @@
 <div class="container">
-  <div class="card mx-auto" style="margin-top: 100px;margin-bottom:200px; width:90%">
+  <div class="card mx-auto" style="margin-top: 100px;margin-bottom:200px; width:95%">
     <div class="card-header">
       Buku Anda
     </div>
@@ -15,8 +15,8 @@
         </div>
       </div>
     <?php endif; ?>
-    <div class="card-body">
-      <table class="table table-border table-bordered table-striped">
+    <div class="card-body table-responsive">
+      <table class="table table-bordered table-striped ">
         <thead>
           <tr>
             <th>No</th>
@@ -24,20 +24,27 @@
             <th>Pengarang</th>
             <th>Tanggal Pinjam</th>
             <th>Tanggal Kembali</th>
-            <th>Pilih</th>
+            <th colspan="2" class="text-center">Pilih</th>
           </tr>
         </thead>
         <tbody>
          <?php $no=1; foreach( $dataPinjam as $p ): ?>
-          <td><?= $no++;?></td>
-          <td><?= $p['judul_buku'];?></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td><a href="<?= base_url('dashboard/transaksi_aksi');?>"></a></td>
+          <tr>
+            <td><?= $no++;?></td>
+            <td><?= $p['judul_buku'];?></td>
+            <td><?= $p['pengarang'];?></td>
+            <td><?= $p['tanggal_pinjam'];?></td>
+            <td><?= $p['tanggal_kembali'];?></td>
+            <td><a class="btn btn-sm btn-danger" href="<?= base_url('dashboard/transaksi_aksi');?>">Hapus</a></td>
+            <td><a class="btn btn-sm btn-primary" href="<?= base_url('dashboard/transaksi_aksi');?>">Detail</a></td>
+          </tr>
          <?php endforeach; ?>
         </tbody>
       </table>
+    </div>
+    <div class="card-footer text-right">
+      <a href="<?= base_url('dashboard');?>" class="btn btn-sm btn-primary">Pinjam Lagi</a>
+      <button class="btn btn-sm btn-success">Lanjutkan</button>
     </div>
   </div>
 </div>
