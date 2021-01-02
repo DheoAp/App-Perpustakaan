8d458da4a11a98ee48183be1df63d3f03c9fe46a
Dheo Apriansyah <dheoap21@gmail.com>
2021-01-02 14:46:52 +0700

    membuat function pinjam_buku dan aksi_pinjam_buku

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -30,9 +30,35 @@ class Dashboard extends CI_Controller{
 		$this->load->view('anggota/detail_buku',$data);
 		$this->load->view('templates_anggota/footer');
 	}
-	public function pinjam_buku()
+
+	public function pinjam_buku($id)
 	{
+		$data['dataPinjam'] = $this->M_perpus->getById($id)->result_array();
+		$data['title'] = "pinjam_buku";
+		$this->load->view('templates_anggota/header',$data);
+		// $this->load->view('anggota/keranjang_buku');
 		$this->load->view('anggota/pinjam_buku');
+		$this->load->view('templates_anggota/footer');
 	}
+	public function aksi_pinjam_buku()
+	{
+		$data = [
+			'id_anggota' => $this->session->userdata('id_anggota'),
+			'id_buku' => htmlspecialchars($this->input->post('id_buku',true)),
+			'tanggal_pinjam' => htmlspecialchars($this->input->post('tanggal_pinjam',true)),
+			'tanggal_kembali' => htmlspecialchars($this->input->post('tanggal_kembali',true)),
+			'tanggal_dikembalikan' => '-',
+			'total_denda' => '0',
+			'status_peminjaman' => 'Belum Selesai',
+			'status_pengembalian' => 'Belum di Kembali',
+			'keperluan' => htmlspecialchars($this->input->post('keperluan',true)),
+		];
+		$this->M_perpus->insert_data('keranjang',$data);
+		$this->session->set_flashdata('pesan','Buku Berhasil di Tambahkan, cek di keranjang.');
+		redirect('dashboard');
+
+	}
+
+		
 
 } // akhir class
