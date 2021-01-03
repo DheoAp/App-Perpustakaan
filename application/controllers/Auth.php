6320823f46fce22e31b36b8185696e43b5a50c2d
Dheo Apriansyah <dheoap21@gmail.com>
2021-01-03 14:20:30 +0700

    merubah redirect _login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -39,7 +39,7 @@ class Auth extends CI_Controller{
         ];
         $this->session->set_userdata($data);
         if($user['role_id'] == 1){
-          redirect('admin');
+          redirect('Admin/dashboard');
         }else{
           redirect('dashboard');
         }
