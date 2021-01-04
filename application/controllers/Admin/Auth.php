2f05667cd04815b97c699bebfaea1f513a00bd00
Dheo Apriansyah <dheoap21@gmail.com>
2021-01-04 14:01:25 +0700

    membuat function cek_login

--- application/controllers/Admin/Auth.php
+++ application/controllers/Admin/Auth.php
@@ -4,8 +4,16 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller{
 
+
+  private function cek_login()
+  {
+    if($this->session->userdata('email')){
+      redirect('admin/dashboard');
+    }
+  }
   public function login()
   {
+    $this->cek_login();
     $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
     $this->form_validation->set_rules('password', 'Password', 'required');
 
