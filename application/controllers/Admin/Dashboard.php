cfdeffa232297e01b80de1efac175d1d29f8fd0a
Dheo Apriansyah <dheoap21@gmail.com>
2021-01-06 11:38:39 +0700

    merubah dan merapihkan function

--- application/controllers/Admin/Dashboard.php
+++ application/controllers/Admin/Dashboard.php
@@ -1,51 +1,35 @@
 <?php
-  class Dashboard extends CI_Controller{
-   function __construct()
-   {
-     parent::__construct();
-     //cek login
-     cek_login();
-   }
+class Dashboard extends CI_Controller{
+  function __construct()
+  {
+    parent::__construct();
+    //cek login
+    cek_login();
+  }
 
   
-   public function index()
-   {
-     // memparsing data yang kita ambil dari database
-     $data['peminjaman'] = $this->db->query("select * from peminjaman order by id_pinjam desc limit 10")->result();
+  public function index()
+  {
+    // memparsing data yang kita ambil dari database
+    $data['peminjaman'] = $this->db->query("select * from peminjaman order by id_pinjam desc limit 10")->result();
     //  $data['peminjaman'] = $this->M_perpus->pinjam('peminjaman')->result();
-     $data['anggota'] = $this->db->query("select * from anggota order by id_anggota desc limit 10")->result();
-     $data['buku'] = $this->db->query('select * from buku order by id_buku desc limit 10')->result();
-     $data['peminjaman'] = $this->db->query('select * from keranjang order by id_anggota desc limit 10')->result();
+    $data['anggota'] = $this->db->query("select * from anggota order by id_anggota desc limit 10")->result();
+    $data['buku'] = $this->db->query('select * from buku order by id_buku desc limit 10')->result();
+    $data['peminjaman'] = $this->db->query('select * from peminjaman order by id_anggota desc limit 10')->result();
 
-     $data['peminjaman'] = $this->db->query("SELECT * FROM keranjang p, buku b, anggota a WHERE p.id_buku=b.id_buku And p.id_anggota=a.id_anggota")->result_array();
+    $data['peminjaman'] = $this->db->query("SELECT * FROM peminjaman p, buku b, anggota a WHERE p.id_buku=b.id_buku And p.id_anggota=a.id_anggota")->result_array();
 
-     $this->load->view('templates_admin/header');
-     $this->load->view('templates_admin/sidebar');
-     $this->load->view('admin/index',$data);
-     $this->load->view('templates_admin/footer');
-    
-   }
+    $this->load->view('templates_admin/header');
+    $this->load->view('templates_admin/sidebar');
+    $this->load->view('admin/index',$data);
+    $this->load->view('templates_admin/footer');
+  
+  }
 
-   
 
-   function buku()
-   {
-     $data['buku'] = $this->M_perpus->get_data('buku')->result();
-     $this->load->view('templates/header');
-     $this->load->view('admin/buku',$data);
-     $this->load->view('templates/footer');
-   }  
-   public function tambah_buku()
-   {
-     $data['kategori'] = $this->M_perpus->get_data('kategori')->result();
-
-     $this->load->view('templates_admin/header');
-     $this->load->view('templates_admin/sidebar');
-     $this->load->view('admin/tambah_buku',$data);
-     $this->load->view('templates_admin/footer');
-   }
-   private function _rules()
-   {
+  // Function Data Buku
+  private function _rules()
+  {
     $this->form_validation->set_rules('id_kategori', 'Id Kategori', 'required');
     $this->form_validation->set_rules('judul_buku', 'Judul Buku', 'required');
     $this->form_validation->set_rules('pengarang', 'Pengarang', 'required');
@@ -55,9 +39,26 @@
     $this->form_validation->set_rules('jumlah_buku', 'Jumlah Buku', 'required');
     $this->form_validation->set_rules('lokasi', 'Lokasi', 'required');
     $this->form_validation->set_rules('status_buku', 'Status Buku', 'required');
-   }
-   public function tambah_buku_aksi()
-   {
+  }
+  function buku()
+  {
+    $data['buku'] = $this->M_perpus->data_buku()->result_array();
+    $this->load->view('templates_admin/header',$data);
+    $this->load->view('templates_admin/sidebar');
+    $this->load->view('admin/data_buku');
+    $this->load->view('templates_admin/footer');
+  }  
+  public function tambah_buku()
+  {
+    $data['kategori'] = $this->M_perpus->get_data('kategori')->result();
+
+    $this->load->view('templates_admin/header');
+    $this->load->view('templates_admin/sidebar');
+    $this->load->view('admin/tambah_buku',$data);
+    $this->load->view('templates_admin/footer');
+  }
+  public function tambah_buku_aksi()
+  {
     $this->_rules();
 
     if($this->form_validation->run() == false){
@@ -97,22 +98,19 @@
       $this->session->set_flashdata('pesan','Buku Berhasil di Tambahkan');
       redirect('admin');
     }
-   }
-
-
-   function edit_buku($id)
-   {
-     $where = array('id_buku' => $id);
-     $data['buku'] = $this->db->query("SELECT * FROM buku B, kategori K WHERE B.id_kategori=K.id_kategori AND B.id_buku ='$id'")->result();
-     $data['kategori'] = $this->M_perpus->get_data('kategori')->result();
-     // $data['buku'] = $this->M_perpus->get_data('kategori')->result(?);
-     $this->load->view('templates/header');
-     $this->load->view('admin/edit_buku',$data);
-     $this->load->view('templates/footer');
-   }
-
-   function update_buku()
-   {
+  }
+  function edit_buku($id)
+  {
+    $where = array('id_buku' => $id);
+    $data['buku'] = $this->db->query("SELECT * FROM buku B, kategori K WHERE B.id_kategori=K.id_kategori AND B.id_buku ='$id'")->result();
+    $data['kategori'] = $this->M_perpus->get_data('kategori')->result();
+    // $data['buku'] = $this->M_perpus->get_data('kategori')->result(?);
+    $this->load->view('templates/header');
+    $this->load->view('admin/edit_buku',$data);
+    $this->load->view('templates/footer');
+  }
+  function update_buku() /* Aksi */
+  {
     $id = $this->input->post('id');
     $id_kategori = $this->input->post('id_kategori');
     $judul = $this->input->post('judul_buku');
@@ -135,76 +133,96 @@
     $this->form_validation->set_rules('status','Status Buku','required');
 
     if($this->form_validation->run() != false){
-        $config['upload_path'] = './assets/upload/';
-        $config['allowed_types'] = 'jpg|png|jpeg';
-        $config['max_size'] = '2048';
-        $config['file_name'] = 'gambar'.time();
+      $config['upload_path'] = './assets/upload/';
+      $config['allowed_types'] = 'jpg|png|jpeg';
+      $config['max_size'] = '2048';
+      $config['file_name'] = 'gambar'.time();
 
-        $this->load->library('upload',$config);
+      $this->load->library('upload',$config);
 
-        $where = array('id_buku' => $id);
-        $data = array(
-          'id_kategori' =>$id_kategori,
-          'judul_buku' =>$judul,
-          'pengarang' =>$pengarang,
-          'penerbit' =>$penerbit,
-          'thn_terbit' =>$thn_terbit,
-          'isbn' =>$isbn,
-          'jumlah_buku' =>$jumlah_buku,
-          'lokasi' =>$lokasi,
-          // 'gambar' =>$image['file_name'],
-          'status_buku' =>$status
+      $where = array('id_buku' => $id);
+      $data = array(
+        'id_kategori' =>$id_kategori,
+        'judul_buku' =>$judul,
+        'pengarang' =>$pengarang,
+        'penerbit' =>$penerbit,
+        'thn_terbit' =>$thn_terbit,
+        'isbn' =>$isbn,
+        'jumlah_buku' =>$jumlah_buku,
+        'lokasi' =>$lokasi,
+        // 'gambar' =>$image['file_name'],
+        'status_buku' =>$status
         );
+        if($this->upload->do_upload('foto')){
+          //proses upload Gambar
+          $image = $this->upload->data();
+          unlink('assets/upload/'.$this->input->post('old_pict',TRUE));
+          $data['gambar'] = $image['file_name'];
 
-         if($this->upload->do_upload('foto')){
-            //proses upload Gambar
-            $image = $this->upload->data();
-            unlink('assets/upload/'.$this->input->post('old_pict',TRUE));
-            $data['gambar'] = $image['file_name'];
+          $this->M_perpus->update_data($data,$where,'buku');
+        }else{
+         $this->M_perpus->update_data($data,$where,'buku');
+        }
 
-            $this->M_perpus->update_data($data,$where,'buku');
-          } else{
-            $this->M_perpus->update_data($data,$where,'buku');
-          }
+      $this->M_perpus->update_data($data,$where,'buku');
+      redirect(base_url().'admin/buku');
+    }else{
+      $where = array('id_buku' =>$id);
+      $data['buku'] = $this->db->query("SELECT * FROM buku B, kategori K WHERE B.id_kategori=K.id_kategori AND B.id_buku='$id'")->result();
+      $data['kategori'] = $this->M_perpus->get_data('kategori')->result();
+      $this->load->view('admin/header');
+      $this->load->view('admin/editbuku',$data);
+      $this->load->view('admin/footer');
+    }
+  }
+  public function hapus_buku($id)
+  {
+    $this->M_perpus->hapus_buku('buku',$id);
+    $this->session->set_flashdata('pesan','Buku Berhasil di hapus');
+    redirect('admin');
+  }
 
-        $this->M_perpus->update_data($data,$where,'buku');
-        redirect(base_url().'admin/buku');
-      } else{
-          $where = array('id_buku' =>$id);
-          $data['buku'] = $this->db->query("SELECT * FROM buku B, kategori K WHERE B.id_kategori=K.id_kategori AND B.id_buku='$id'")->result();
-          $data['kategori'] = $this->M_perpus->get_data('kategori')->result();
-          $this->load->view('admin/header');
-          $this->load->view('admin/editbuku',$data);
-          $this->load->view('admin/footer');
-      }
-   }
 
-   public function hapus_buku($id)
-   {
-     $this->M_perpus->hapus_buku('buku',$id);
-     $this->session->set_flashdata('pesan','Buku Berhasil di hapus');
-     redirect('admin');
-   }
+  // Function Data Anggota
+  function anggota()
+  {
+    $data['anggota'] = $this->M_perpus->getData('anggota');
+    $this->load->view('templates_admin/header',$data);
+    $this->load->view('templates_admin/sidebar');
+    $this->load->view('admin/data_anggota');
+    $this->load->view('templates_admin/footer');
+  }
 
-   public function data_pinjam()
-   {
+
+  // Function Data Peminjaman
+  function peminjaman()
+  {
+    $data['peminjaman'] = $this->M_perpus->data_peminjaman()->result_array();
+    $this->load->view('templates_admin/header',$data);
+    $this->load->view('templates_admin/sidebar');
+    $this->load->view('admin/data_peminjaman');
+    $this->load->view('templates_admin/footer');
+  } 
+
+  // Function transaksi/pinjam
+  public function data_pinjam()
+  {
     $data['buku'] = $this->M_perpus->get_data('buku')->result();
     $this->load->view('templates/header');
     $this->load->view('admin/buku',$data);
     $this->load->view('templates/footer');
-   }
-
-   public function buku_kembali($id)
-   {
+  }
+  public function buku_kembali($id)
+  {
     $where = ['id_pinjam' => $id];
-    $data['buku_kembali'] = $this->db->query("SELECT * FROM keranjang k, anggota a, buku b WHERE id_pinjam='$id' AND k.id_anggota=a.id_anggota AND k.id_buku=b.id_buku")->result_array();
+    $data['buku_kembali'] = $this->db->query("SELECT * FROM peminjaman k, anggota a, buku b WHERE id_pinjam='$id' AND k.id_anggota=a.id_anggota AND k.id_buku=b.id_buku")->result_array();
     $this->load->view('templates_admin/header',$data);
     $this->load->view('templates_admin/sidebar');
     $this->load->view('admin/buku_kembali');
     $this->load->view('templates_admin/footer');
-   }
-   public function buku_kembali_aksi()
-   {
+  }
+  public function buku_kembali_aksi()
+  {
     $id                     = $this->input->post('id_pinjam');
     $id_buku                = $this->input->post('id_buku');
     $tanggal_kembali        = $this->input->post('tanggal_kembali');
@@ -228,56 +246,22 @@
     $where = [
       'id_pinjam' => $id
     ];
-    $this->M_perpus->update_data('keranjang',$data,$where);
+    $this->M_perpus->update_data('peminjaman',$data,$where);
 
     // Merubah status buku
     $status = [
-			'status_buku' => '1'
+      'status_buku' => '1'
     ];
-		$id_buku = [
-			'id_buku' => $id_buku
+    $id_buku = [
+      'id_buku' => $id_buku
     ];
-		$this->M_perpus->update_status_buku('buku',$status,$id_buku);
+    $this->M_perpus->update_status_buku('buku',$status,$id_buku);
     $this->session->set_flashdata('pesan','Buku berhasil di kembalikan');
     redirect('admin/dashboard/#peminjaman');
-   }
+  }
 
 
-   function logout()
-   {
-     // untuk menghapus semua session
-     $this->session->sess_destroy();
-     redirect('admin/auth/login');
-   }
-
-   function ganti_password()
-   {
-     $this->load->view('templates/header');
-     $this->load->view('admin/ganti_password');
-     $this->load->view('templates/footer');
-   }
-
-   function ganti_password_act()
-   {
-     $pass_baru = $this->input->post('pass_baru');
-     $ulang_pass = $this->input->post('ulang_pass');
-
-     $this->form_validation->set_rules('pass_baru', 'Password Baru', 'required|matches[ulang_pass]');
-     $this->form_validation->set_rules('ulang_pass', 'Ulangi Password Baru', 'required');
-
-     if($this->form_validation->run() != false){
-        $data = array('password' => md5($pass_baru));
-        $w = array('id_admin' => $this->session->userdata('id')); // untuk perintah menampilkan session 'id' admin yang sedang login
-
-        $this->M_perpus->update_data($w,$data,'admin');
-        redirect(base_url().'admin/ganti_password?pesan=berhasil');
-     }else{
-       $this->load->view('templates/header');
-       $this->load->view('admin/ganti_password');
-       $this->load->view('templates/footer');
-     }
-   }
-
+  // Daftar
   public function daftar()
   {
     $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[admin.email]');
@@ -305,8 +289,41 @@
       $this->session->set_flashdata('daftar','Berhasil di daftarkan.');
       redirect('admin/auth/login');
     }
+  }
 
-    
+
+  // Auth
+  function logout()
+  {
+    // untuk menghapus semua session
+    $this->session->sess_destroy();
+    redirect('admin/auth/login');
+  }
+  function ganti_password()
+  {
+    $this->load->view('templates/header');
+    $this->load->view('admin/ganti_password');
+    $this->load->view('templates/footer');
+  }
+  function ganti_password_act()
+  {
+    $pass_baru = $this->input->post('pass_baru');
+    $ulang_pass = $this->input->post('ulang_pass');
+
+    $this->form_validation->set_rules('pass_baru', 'Password Baru', 'required|matches[ulang_pass]');
+    $this->form_validation->set_rules('ulang_pass', 'Ulangi Password Baru', 'required');
+
+    if($this->form_validation->run() != false){
+      $data = array('password' => md5($pass_baru));
+      $w = array('id_admin' => $this->session->userdata('id')); // untuk perintah menampilkan session 'id' admin yang sedang login
+
+      $this->M_perpus->update_data($w,$data,'admin');
+      redirect(base_url().'admin/ganti_password?pesan=berhasil');
+    }else{
+      $this->load->view('templates/header');
+      $this->load->view('admin/ganti_password');
+      $this->load->view('templates/footer');
+    }
   }
 
 }// akhir class
