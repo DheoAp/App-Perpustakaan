fff9df076aca69c90f42f7a4fee8bb1489402a45
Dheo Apriansyah <dheoap21@gmail.com>
2021-01-09 09:07:45 +0700

    membuat form edit buku

--- application/views/admin/edit_buku.php
+++ application/views/admin/edit_buku.php
@@ -1,94 +1,120 @@
-<div class="page-header mb-5" style="text-align:center;" >
-  <h3>Edit Buku</h3>
-</div>
-<div class="row" style="margin: 50px;">
+<div id="layoutSidenav_content">
+  <main>
+    <div class="container-fluid mt-3">
 
-  <div class="col-md">
-    <?php foreach( $buku  as $b ): ?>
-    <form action="<?= base_url().'admin/update_buku'?>" method="post" enctype="multipart/form-data">
-    <!-- Kategori -->
-    <div class="form-group">
-      <label>Kategori</label>
-      <select class="form-control" name="id_kategori">
-          <option value="<?= $b->id_kategori;?>"><?= $b->nama_kategori;?></option>
-        <?php foreach( $kategori  as $k ): ?>
-          <option value="<?= $k->id_kategori;?>"><?= $k->nama_kategori;?></option>
-        <?php endforeach; ?>
-      </select>
-      <?= form_error('id_kategori');?>
-    </div>
-    <!-- Judul Buku -->
-    <div class="form-group">
-      <label for="judul_buku">Judul Buku</label>
-      <input type="hidden" name="id" value="<? $b->id_buku;?>">
-      <input class="form-control" type="text" name="judul_buku" id="judul_buku" value="<?= $b->judul_buku;?>">
-      <?= form_error('judul_buku');?>
-    </div>
-    <!-- Pengarang -->
-    <div class="form-group">
-      <label for="pengarang">Pengarang</label>
-      <input class="form-control" type="text" name="pengarang" id="pengarang" value="<?= $b->pengarang;?>">
-      <?= form_error('pengarang');?>
-    </div>
-    <!-- Penerbit -->
-    <div class="form-group">
-      <label for="penerbit">Penerbit</label>
-      <input class="form-control" type="text" name="penerbit" id="penerbit" value="<?= $b->penerbit;?>">
-      <?= form_error('penerbit');?>
-    </div>
-    <!-- Tahun Terbit -->
-    <div class="form-group">
-      <label for="thn_terbit">Tahun Terbit</label>
-      <input class="form-control" type="date" name="thn_terbit" id="thn_terbit" value="<?= $b->thn_terbit;?>">
-      <?= form_error('thn_terbit');?>
-    </div>
+      <div class="card-body">
+      <?= form_open_multipart('admin/dashboard/update_buku_aksi'); ?>
+        <div class="card">
+          <div class="card-header">
+            Tambah Buku
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <?php foreach( $buku as $b ): ?>
+              <!-- Col 1 -->
+              <div class="col-md">
+                <!-- Kategori -->
+                <div class="form-group">
+                  <label>Kategori</label>
+                  <select class="form-control" name="id_kategori">
+                    <option value="<?= $b['nama_kategori'];?>"><?= $b['nama_kategori'];?></option>
+                    <?php foreach( $kategori  as $k ): ?>
+                    <option value="<?= $k['id_kategori'];?>"><?= $k['nama_kategori'];?></option>
+                    <?php endforeach; ?>
+                  </select>
+                  <?= form_error('id_kategori', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <!-- Judul Buku -->
+                <div class="form-group">
+                  <label for="judul_buku">Judul Buku</label>
+                  <input value="<?= $b['judul_buku'];?>" class="form-control" type="text" name="judul_buku" id="judul_buku">
+                  <input type="hidden" name="id_buku" value="<?= $b['id_buku'];?>">
+                  <input type="hidden" name="id_kategori" value="<?= $b['id_kategori'];?>">
+                  <?= form_error('judul_buku', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <!-- Pengarang -->
+                <div class="form-group">
+                  <label for="pengarang">Pengarang</label>
+                  <input value="<?= $b['pengarang'];?>" class="form-control" type="text" name="pengarang" id="pengarang">
+                  <?= form_error('pengarang', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <!-- Penerbit -->
+                <div class="form-group">
+                  <label for="penerbit">Penerbit</label>
+                  <input value="<?= $b['penerbit'];?>" class="form-control" type="text" name="penerbit" id="penerbit">
+                  <?= form_error('penerbit', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <!-- Tahun Terbit -->
+                <div class="form-group">
+                  <label for="thn_terbit">Tahun Terbit</label>
+                  <input value="<?= $b['thn_terbit'];?>" class="form-control" type="date" name="thn_terbit" id="thn_terbit">
+                  <?= form_error('thn_terbit', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
 
-  </div>
+              </div>
+              <!-- End Col 1 -->
 
-  <div class="col-md">
-    <!-- ISBN -->
-    <div class="form-group">
-      <label for="isbn">ISBN</label>
-      <input class="form-control" type="text" name="isbn" id="isbn" value="<?= $b->isbn;?>">
-      <?= form_error('isbn');?>
-    </div>
-    <!-- Jumlah Buku -->
-    <div class="form-group">
-      <label for="jumlah_buku">Jumlah Buku</label>
-      <input class="form-control" type="text" name="jumlah_buku" id="jumlah_buku" value="<?= $b->jumlah_buku;?>">
-      <?= form_error('jumlah_buku');?>
-    </div>
-    <!-- Lokasi -->
-    <div class="form-group">
-      <label for="lokasi">Lokasi</label>
-      <input class="form-control" class="form-control" type="text" name="lokasi" id="lokasi" value="<?= $b->lokasi;?>">
-      <?= form_error('lokasi');?>
-    </div>
-    <!-- Status Buku -->
-    <div class="form-group">
-      <label>Status Buku</label>
-      <select name="status" class="form-control">
-        <option <?php if($b->status_buku == "1"){echo "selected='selected'";} echo $b->status_buku;?> value="1">Tersedia</option>
-        <option <?php if($b->status_buku == "0"){echo "selected='selected'";} echo $b->status_buku;?> value="0">Sedang di pinjam</option>
-      </select>
-      <?= form_error('status');?>
-    </div>
-    <!-- Gambar -->
-    <div class="form-group">
-      <label for="gambar">Gambar</label>
-      <?php
-        if(isset($b->gambar)){
-          echo '<input type="hidden" name="old_pict" value="'.$b->gambar.'">';
-          echo '<img src="'.base_url().'assets/upload/'.$b->gambar.'" width="30%">';
-        }
-      ?>
-      <input class="form-control" type="file" name="foto" id="gambar">
-    </div>
-    <div class="form-group">
-      <button class="btn btn-primary" type="submit" name="submit">Update Data</button>
-    </div> 
-    </form>
-    <?php endforeach; ?>
-  </div>
+              <!-- Col 2 -->
+              <div class="col-md">
+                <!-- ISBN -->
+                <div class="form-group">
+                  <label for="isbn">ISBN</label>
+                  <input value="<?= $b['isbn'];?>" class="form-control" type="text" name="isbn" id="isbn">
+                  <?= form_error('isbn', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <!-- Jumlah Buku -->
+                <div class="form-group">
+                  <label for="jumlah_buku">Jumlah Buku</label>
+                  <input value="<?= $b['jumlah_buku'];?>" class="form-control" type="text" name="jumlah_buku" id="jumlah_buku">
+                  <?= form_error('jumlah_buku', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <!-- Lokasi -->
+                <div class="form-group">
+                <label>Lokasi Buku</label>
+                  <select name="lokasi" class="form-control">
+                    <option value="<?= $b['lokasi'];?>"><?= $b['lokasi'];?></option>
+                    <option value="Rak 1">Rak 1</option>
+                    <option value="Rak 2">Rak 2</option>
+                    <option value="Rak 3">Rak 3</option>
+                  </select>
+                  <?= form_error('lokasi', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <!-- Status Buku -->
+                <div class="form-group">
+                  <label>Status Buku</label>
+                  <select name="status_buku" class="form-control">
+                    <option value="<?= $b['status_buku'];?>">
+                      <?php if( $b['status_buku'] == "1" ): ?>
+                          <?= "Tersedia";?>
+                      <?php else: ?>
+                        <?= "Tidak Tersedia";?>
+                      <?php endif; ?>
+                    </option>
+                    <option value="1">Tersedia</option>
+                    <option value="0">Tidak Tersedia</option>
+                  </select>
+                  <?= form_error('status_buku', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <!-- Gambar -->
+                <div class="form-group">
+              <label for="gambar">Gambar</label>
+                 <img src="<?= base_url('assets/upload/'.$b['gambar']);?>" width="30%">
+              <input class="form-control" type="file" name="gambar" id="gambar">
+            </div>
+              </div>        
+              <!-- End Col 2 -->
+            <?php endforeach; ?>
+            </div>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-primary" type="submit">Update Buku</button>
+            <a href="<?= base_url('admin/dashboard/buku');?>" class="btn btn-danger">Kembali</a>
+          </div>
+        </div>
+        
+      <?= form_close();?>
+      </div>
 
+    </div>
+  </main>
 </div>
